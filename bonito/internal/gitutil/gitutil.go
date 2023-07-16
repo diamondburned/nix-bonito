9f0fc4f941b9b5c1031a7a0a1f1bd5752b7a57c1
diamondburned <diamond@arikawa-hi.me>
2023-07-16 01:20:59 -0700

    Fix commit hash not resolving

--- bonito/internal/gitutil/gitutil.go
+++ bonito/internal/gitutil/gitutil.go
@@ -2,6 +2,7 @@ package gitutil
 
 import (
 	"context"
+	"encoding/hex"
 	"fmt"
 	"strings"
 
@@ -19,8 +20,19 @@ func RefCommit(ctx context.Context, remote, ref string) (string, error) {
 
 	parts := strings.Fields(out)
 	if len(parts) < 1 {
+		// ref might be a commit hash. Check if it's a valid hex string.
+		if len(ref) <= 40 && isValidCommitHash(ref) {
+			return ref, nil
+		}
 		return "", fmt.Errorf("ref %q not found", ref)
 	}
 
 	return parts[0], nil
+}
+
+func isValidCommitHash(hash string) bool {
+	// Round down the hex string so that it's a multiple of 2.
+	l := len(hash) - len(hash)%2
+	_, err := hex.DecodeString(hash[:l])
+	return err == nil
 }
